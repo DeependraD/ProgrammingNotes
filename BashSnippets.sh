3df0949478ad2aa3e074002ff3b4df591a3e4b5f
Deependra Dhakal <dean.ionized@gmail.com>
2020-03-30 12:56:23 +0545

    Merged linus and git folder

---
+++ BashSnippets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# viewing environment variables
+echo "The value of the home is: "
+echo $HOME
+
+# issue a command
+# echo "The output of the pwd command is: "
+# pwd
+
+# that's boring, grab output and make it readable
+# echo "The value of the pwd command is $(pwd)"
+
+# assign command output to a variable
+# output=$(pwd)
+# echo "The value of the output variable is ${output}
+
