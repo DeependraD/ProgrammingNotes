e1afddad41e188ff43b50194b7789b7a88d42dd0
Deependra Dhakal <dean.ionized@gmail.com>
2022-01-31 18:06:17 +0545

    Updated slideshow conversion and orientation
    Now, exif data can be retrieved and viewed from R directly. Also mogrify command (to auto rotate) is issued from R.
    
    Only the ffmpeg script is run via console. New ffmpeg script to auto correct for variable input frame pixel by image padding is included in example.

--- ffmpeg-image-slideshow-list_generate.R
+++ ffmpeg-image-slideshow-list_generate.R
@@ -1,3 +1,47 @@
+require(tidyverse)
+
+# write name of images to a text file
 paste0("file '", list.files("./", pattern = "*.jpg", full.names = TRUE)) %>%
   stringr::str_c("'") %>%
   writeLines("./ffmpeg-image-slideshow-file-list.txt", sep = "\n")
+
+# read exif data
+
+file_list <- list.files("./", pattern = "*.jpg", full.names = FALSE)
+
+# system2 command encourages single command use only
+system2(command = "identify", args = c("-format '%[EXIF:*]'", file_list[1]),
+        stdout = TRUE) %>% 
+  str_remove_all("^\\'") %>% 
+  strsplit(split = "=", fixed = TRUE) %>% 
+  discard(~is_empty(.x)) %>% 
+  map(~set_names(.x[[2]], .x[[1]])) %>% 
+  unlist() %>% 
+  bind_rows()
+  
+# but let us map it over all files
+all_image_exifs <- map_dfr(file_list, ~system2(command = "identify", args = c("-format '%[EXIF:*]'", .x),
+                                 stdout = TRUE) %>% 
+          str_remove_all("^\\'") %>% 
+          strsplit(split = "=", fixed = TRUE) %>% 
+          discard(~is_empty(.x)) %>% 
+          map(~set_names(.x[[2]], .x[[1]])) %>% 
+          unlist())
+
+bad_orientation <- all_image_exifs %>% 
+  mutate(file_name = file_list) %>% 
+  filter(`exif:Orientation` != 1) %>% 
+  pull(file_name)
+
+# reset orientation value in exif metadata
+walk(paste0('mogrify -auto-orient ', bad_orientation), ~system(command = .x))
+
+# check the auto orientation result
+map_dfr(bad_orientation, ~system2(command = "identify", args = c("-format '%[EXIF:*]'", .x),
+                            stdout = TRUE) %>% 
+          str_remove_all("^\\'") %>% 
+          strsplit(split = "=", fixed = TRUE) %>% 
+          discard(~is_empty(.x)) %>% 
+          map(~set_names(.x[[2]], .x[[1]])) %>% 
+          unlist()) %>% 
+  View()
